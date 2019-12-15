cd7fe528bc3f84eadb05165f62b2b93d9845d2b5
Lefteris <el.avramidis@gmail.com>
2019-12-15 19:00:34 +0000

    Added Pyplot class

---
+++ include/pyplot/Pyplot.hpp
@@ -0,0 +1,17 @@
+//
+// Created by elavram on 15/12/2019.
+//
+
+#ifndef CXXPLOT_PYPLOT_HPP
+#define CXXPLOT_PYPLOT_HPP
+
+namespace cxxplot {
+    class Pyplot {
+    public:
+        Pyplot();
+
+        ~Pyplot();
+    };
+}
+
+#endif
---
+++ source/pyplot/Pyplot.cpp
@@ -0,0 +1,12 @@
+//
+// Created by elavram on 15/12/2019.
+//
+
+#include "pyplot/Pyplot.hpp"
+
+namespace cxxplot {
+
+    Pyplot::Pyplot() { };
+
+    Pyplot::~Pyplot() { };
+}
