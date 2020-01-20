10f06ada2b1228749b388409167cfbaf3e0bf360
Eleftherios Avramidis <el.avramidis@gmail.com>
2020-01-20 14:13:58 +0000

    Added functions to set the axis limits

--- include/pyplot/Pyplot.hpp
+++ include/pyplot/Pyplot.hpp
@@ -14,29 +14,35 @@
 #include <stdexcept>
 
 namespace cxxplot {
-    template<class inputType>
-    class CXXPLOT_EXPORT Pyplot : public Matplotlib<inputType> {
-    public:
-        PyObject* xlabel;
-        PyObject* ylabel;
-        PyObject* show;
+	template<class inputType>
+	class CXXPLOT_EXPORT Pyplot : public Matplotlib<inputType> {
+	public:
+		PyObject* xlabel;
+		PyObject* ylabel;
+		PyObject* show;
 
-        Pyplot();
+		Pyplot();
 
-        ~Pyplot();
+		~Pyplot();
 
-        void
-        set_xlabel(std::string const& label, std::map<std::string, std::string> const& args = {});
+		void
+		set_xlabel(std::string const& label, std::map<std::string, std::string> const& args = {});
 
-        void
-        set_ylabel(std::string const& label, std::map<std::string, std::string> const& args = {});
+		void
+		set_ylabel(std::string const& label, std::map<std::string, std::string> const& args = {});
 
-        void
-        show_plot();
+		void
+		show_plot();
 
-        void
-        savefig(std::string const& fname);
-    };
+		void
+		savefig(std::string const& fname);
+
+		void set_xlim(double x_min, double x_max);
+		void set_ylim(double y_min, double y_max);
+
+	private:
+		void set_x_or_y_lim(double min_value, double max_value, std::string lim_type);
+	};
 }
 
 #endif
