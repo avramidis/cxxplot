6e035600510ed25bc89a093528ba1d7da10dcb68
Eleftherios Avramidis <el.avramidis@gmail.com>
2020-01-23 12:34:56 +0000

    Removed wrongly places nodiscards

--- include/generic/PythonCalls.hpp
+++ include/generic/PythonCalls.hpp
@@ -23,11 +23,11 @@ namespace cxxplot {
         [[nodiscard]] static PyObject *
         get_pyobject_function_by_string(PyObject *parent_module, const std::string &str);
 
-        [[nodiscard]] static void pyobject_call_with_checks(PyObject *object_to_call, PyObject *args, PyObject *kwargs);
+        static void pyobject_call_with_checks(PyObject *object_to_call, PyObject *args, PyObject *kwargs);
 
-        [[nodiscard]] static void pyobject_callobject_with_checks(PyObject *object_to_call);
+        static void pyobject_callobject_with_checks(PyObject *object_to_call);
 
-        [[nodiscard]] static void pyobject_callobject_with_checks(PyObject *object_to_call, PyObject *args);
+        static void pyobject_callobject_with_checks(PyObject *object_to_call, PyObject *args);
     };
 }
 
